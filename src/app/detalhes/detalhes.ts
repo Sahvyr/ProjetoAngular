db1469ce6807bc00864af9adf7734aeacac2ced4
Thiago Rodrigues <thiago.rodrigues365701@gmail.com>
2025-09-29 13:38:42 -0300

    feat~ detalhes atualizado

--- src/app/detalhes/detalhes.ts
+++ src/app/detalhes/detalhes.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalhes',
   standalone: false,
   templateUrl: './detalhes.html',
-  styleUrl: './detalhes.css'
+  styleUrl: './detalhes.css',
 })
 export class Detalhes {
+  constructor(private route: ActivatedRoute) {}
 
+  idUsuario: any = '';
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      this.idUsuario = params.get('id');
+    });
+  }
 }
